04a1b8770cc742e3160b70b477989c15cacf9678
Alessandro Calbucci <ALESSANDRO.CALBUCCI@local.ittcesena.it>
2021-10-01 09:35:27 +0200

    v1.0: prima versione

---
+++ prova/prova/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+            Array.Sort(array);
+        }
+    }
+}
